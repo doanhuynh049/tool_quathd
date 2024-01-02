62d33cf1c5189090efa2f4aecfc6a6aa54260580
doanhuynh049 <quocthien049@gmail.com>
2024-01-02 12:19:29 +0700

    gier

--- wayland_tool/202.sh
+++
@@ -1,5 +0,0 @@
-echo "Test Case 202: Resume from inactive mode"
-sed -i 's/--idle-time=0/--idle-time=30/' /lib/systemd/system/weston.service
-echo "2. Restart the system with the new configuration."
-reboot
-
--- wayland_tool/script-test.sh
+++
@@ -1,5 +0,0 @@
-
-for i in {10..1}; do
-    echo "Waiting for $i more minute(s). Please continue to observe the VGA screen and console output for any unexpected output."
-    sleep 60
-done
